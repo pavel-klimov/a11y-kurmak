54eb24102d9a6922622519522490b72d262d88dd
Pavel Klimov <pavelklimovmail@yandex.ru>
2021-06-16 19:07:53 +0300

    add more custom properties and start carousel

--- js/main.js
+++ js/main.js
@@ -23,3 +23,34 @@ loginButton.addEventListener('keyup', (evt) => {
     openLoginModalOnClick(evt);
   }
 });
+
+const carousel = document.querySelector('.carousel');
+
+const items = carousel.querySelectorAll('.carousel__item');
+let activeItem = 0;
+
+
+carousel.querySelector('.carousel__item_active')?.classList.remove('carousel__item_active');
+
+// const lastPlaceholder = items[items.length - 1].cloneNode(true);
+// const firstPlaceholder = items[0].cloneNode(true);
+
+// items[0].parentNode.insertAdjacentElement('beforeend', firstPlaceholder);
+// items[0].parentNode.insertAdjacentElement('afterbegin', lastPlaceholder);
+
+
+const nextCarouselStep = function () {
+  items[activeItem].classList.remove('carousel__item_active');
+  items[(activeItem + 1 < items.length) ? activeItem + 1 : 0].classList.remove('carousel__item_next');
+  items[(activeItem - 1 < 0) ? items.length - 1 : activeItem - 1].classList.remove('carousel__item_preview');
+
+  activeItem = (activeItem + 1 < items.length) ? activeItem + 1 : 0;
+
+  items[activeItem].classList.add('carousel__item_active');
+  items[(activeItem + 1 < items.length) ? activeItem + 1 : 0].classList.add('carousel__item_next');
+  items[(activeItem - 1 < 0) ? items.length - 1 : activeItem - 1].classList.add('carousel__item_preview');
+
+  setTimeout(nextCarouselStep, 5000);
+}
+
+nextCarouselStep();
